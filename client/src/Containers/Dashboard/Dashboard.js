1478dbb37d68fda7bd446fa2c541c6634dc209fc
Kritansh <kkritansh7@gmail.com>
2020-04-06 07:51:22 +0530

    adding link to insta

--- client/src/Containers/Dashboard/Dashboard.js
+++ client/src/Containers/Dashboard/Dashboard.js
@@ -103,7 +103,7 @@ class Dashboard extends Component {
                     </div>
 
                     <div>
-                        <div onClick={() => window.open("instagram://send?text=%F0%9F%99%8B%E2%80%8D%E2%99%80 *Best Buddy Challenge 2020* %F0%9F%99%8B%E2%80%8D%E2%99%82%0A How much do you know about me? %E2%98%BA%F0%9F%A4%97%0A%F0%9F%A4%A9%F0%9F%91%87%F0%9F%91%87%F0%9F%91%87%F0%9F%91%87%F0%9F%91%87%F0%9F%A4%A9%0Ahttps://buddymojo.com/match/9fDx")} className={styles.col} style={{ backgroundImage: "linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)" }}><img className={styles.icon} src={instagramIcon} alt="icon" height="30" /> Share</div>
+                        <div onClick={() => window.open("intent://instagram.com/p//#Intent;package=com.instagram.android;scheme=https;end")} className={styles.col} style={{ backgroundImage: "linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)" }}><img className={styles.icon} src={instagramIcon} alt="icon" height="30" /> Share</div>
                         <div onClick={() => window.open("https://www.twitter.com", '_blank')} className={styles.col} style={{ backgroundImage: "linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)" }}><img className={styles.icon} src={instagramIcon} alt="icon" height="30" /> Set to Bio</div>
                     </div>
 
